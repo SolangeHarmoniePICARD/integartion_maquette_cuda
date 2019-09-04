a7c64ee624c2b4a64f534980a43b203d37b8866b
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-09-04 15:29:27 +0200

    Ajout d'un effet Smooth Scroll en JS

--- js/progressBar.js
+++ js/progressBar.js
@@ -24,17 +24,3 @@ $(function() {
   }
 
 });
-
-/*Scrollspy
-$(function () {
-  $('#navbarNav a').on('click', function(e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000, function(){
-      window.location.hash = hash;
-    });
-  });
-});
-*/
